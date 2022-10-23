e0e10a39ff83b46c7b93fc3a2fd9c8b9d205ebc1
Cipto Hadi <ciptohadi79@gmail.com>
2022-10-23 09:35:38 +0700

    use express

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-export default function handler(req, res) {
-  return res.send(`Assalamu'alaikum!`);
-}
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
